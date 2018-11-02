a3a62c88d7033ec3e30fb95a77519d3017d2bff5
桂素伟 <axzxs2001@163.com>
2018-11-02 11:12:01 +0900

    创建StealthQuartz项目。

---
+++ StealthSolution/StealthQuartz/QuartzLib/BackgroundJob.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.Extensions.Logging;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading.Tasks;
+
+namespace StealthQuartz
+{
+    /// <summary>
+    /// 后台任务
+    /// </summary>
+    public class BackgroundJob : IJob
+    {
+        readonly ILogger<BackgroundJob> _logger;
+        readonly  Action<bool> _action;
+
+        public BackgroundJob(ILogger<BackgroundJob> logger, Action<bool> action)
+        {
+            _action = action;
+            _logger = logger;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                if (context.JobDetail is JobDetailImpl)
+                {
+                    var names = (context.JobDetail as Quartz.Impl.JobDetailImpl)?.Name.Split('_');
+                    var method = names.Length > 1 ? names[1] : "";
+                    if (!string.IsNullOrEmpty(method))
+                    {
+                        //todo 这里用什么方法实现自动调用？？？？？？
+                        //_action()
+                        //var methodInfo = _type.GetMethod(method);
+                        //var result = methodInfo.Invoke(_backgroundRepository, new object[0]);
+                        //var runResult = false;
+                        ////成功执行
+                        //if (bool.TryParse(result.ToString(), out runResult) && runResult)
+                        //{
+                        //    _logger.LogInformation($"BackgroundJob.Execute成功，方法：{method}");
+                        //}
+                        //else//不成功执行
+                        //{
+                        //    _logger.LogInformation($"BackgroundJob.Execute失败，方法：{method}");
+                        //}
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                _logger.LogCritical($"{DateTime.Now}:{exc.Message}");
+                if (exc.InnerException != null)
+                {
+                    _logger.LogCritical($"     {DateTime.Now}:{exc.InnerException.Message}");
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ StealthSolution/StealthQuartz/QuartzLib/QuartzServicesUtilities.cs
@@ -0,0 +1,29 @@
+﻿
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StealthQuartz
+{
+    public static class QuartzServicesUtilities
+    {
+        public async static void StartJob<TJob>(IScheduler scheduler, string cronExpression, string methodName)
+            where TJob : IJob
+        {
+
+            var jobName = $"{typeof(TJob).FullName}_{methodName}_{cronExpression}";
+            var job = JobBuilder.Create<TJob>()
+                .UsingJobData("","")
+                .WithIdentity(jobName)
+                .Build();
+            var trigger = TriggerBuilder.Create()
+                .WithIdentity($"{jobName}.trigger")
+                .WithCronSchedule(cronExpression)
+                .StartNow()
+                .Build();
+            await scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
