08f7589c92437f1f37d740d11a2f56e6b8ef0049
桂素伟 <axzxs2001@163.com>
2018-11-06 12:24:46 +0900

    postgre provider init StealthDB

--- StealthSolution/StealthPostgreProvider/PostgreProvider.cs
+++ StealthSolution/StealthPostgreProvider/PostgreProvider.cs
@@ -1,12 +1,45 @@
-﻿using Npgsql;
-
+﻿using Dapper;
+using Npgsql;
 namespace StealthPostgreProvider
 {
+
     public class PostgreProvider
     {
+        readonly string _connectionString;
         public PostgreProvider(string connectionString)
         {
-            var con = new NpgsqlConnection(connectionString);
+            _connectionString = connectionString;
+            InitTable();
+        }
+        /// <summary>
+        /// init table
+        /// </summary>
+        /// <returns></returns>
+        public bool InitTable()
+        {
+            var connBuilder = new NpgsqlConnectionStringBuilder(_connectionString);
+            connBuilder.Database = "postgres";
+            using (var con = new NpgsqlConnection(connBuilder.ConnectionString))
+            {
+                var selectDbSql = "SELECT count(*)  FROM pg_catalog.pg_database where datname='StealthDB';"
+                var result = con.ExecuteScalar<int>(selectDbSql);
+                if (result == 0)
+                {
+                    var createDBSql = "CREATE DATABASE StealthDB OWNER postgres;";
+                    con.Execute(createDBSql);
+                }
+            }
+            using (var con = new NpgsqlConnection(_connectionString))
+            {
+                var selectSql = "select count(*) as sl from pg_class where relname = 'tablename';";
+                var result = con.ExecuteScalar<int>(selectSql);
+                if (result == 0)
+                {
+                    var createTableSql = "";
+                    con.Execute(createTableSql);
+                }
+            }
+            return true;
         }
     }
 }
