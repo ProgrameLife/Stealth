5b98ee8b4757b8a1614c2354142c3426f6ae45fe
桂素伟 <axzxs2001@163.com>
2018-11-11 18:25:32 +0900

    1、add SFTPBackHandle project 2、collating the code hierarch

---
+++ StealthSolution/BackHandles/StealthSFTPBackHandle/SFTPBackHandle.cs
@@ -0,0 +1,13 @@
+﻿using StealthQuartz;
+using System;
+
+namespace StealthSFTPBackHandle
+{
+    public class SFTPBackHandle : IBackHandle
+    {
+        public bool Handle(params object[] parmeters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
